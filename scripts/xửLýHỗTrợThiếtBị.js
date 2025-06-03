0ce4bfd90343728966e4a844d6527e9d99796775
DaoNgocAnh25092004 <161846189+DaoNgocAnh25092004@users.noreply.github.com>
2025-06-03 23:14:36 +0700

    fix resize screen

--- scripts/xửLýHỗTrợThiếtBị.js
+++ scripts/xửLýHỗTrợThiếtBị.js
@@ -3,13 +3,15 @@ function hỗTrợThiếtBị(check) {
     let chiềuRộngMànHình = window.innerWidth;
     let thôngBáoHỗTrợThiếtBị = document.getElementById("hộTrợThiếtBị");
 
-    if (chiềuRộngMànHình < 375) {
-      thôngBáoHỗTrợThiếtBị.classList.remove("hide");
-    }
-
     if (chiềuRộngMànHình >= 450) {
-      thôngBáoHỗTrợThiếtBị.classList.remove("hide");
+      thôngBáoHỗTrợThiếtBị.classList.remove("hide"); // Hiện thông báo
+    } else {
+      thôngBáoHỗTrợThiếtBị.classList.add("hide"); // Ẩn thông báo
     }
   }
 }
 hỗTrợThiếtBị(true);
+
+window.addEventListener("resize", function () {
+  hỗTrợThiếtBị(true);
+});
